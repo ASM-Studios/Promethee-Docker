54fe67e9e1a8a354c5aa8f583166795fdf2b4930
Charles Madjeri <studies@madjeri.com>
2024-05-20 02:02:02 +0200

    wip(wip)

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-from flask import Flask
-from flask_cors import CORS
-
-from routes.routes import routes
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = '*'
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    header['Access-Control-Allow-Methods'] = 'OPTIONS, GET, POST, PUT, DELETE'
+    return response
+
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
